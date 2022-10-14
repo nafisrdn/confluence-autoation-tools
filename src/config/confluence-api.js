642c74def088d7b004bd60d5030468473b497af8
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2022-10-14 11:03:49 +0700

    Initial commit

---
+++ src/config/confluence-api.js
@@ -0,0 +1,4 @@
+module.exports.API_TOKEN = "ODk4MDUxNzI4ODEyOmf3QSbMgvbdDFWbkCwoIGfrZPHs";
+module.exports.API_URL = "https://confluence.pegadaian.co.id/rest/api";
+module.exports.USERNAME = "nafis.vendor.sapti";
+module.exports.PASSWORD = "GuitaFis14";
