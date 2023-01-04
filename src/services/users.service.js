d121e84e584f1c6ccb638df47912ac5528c7f4db
Nafis Fadhlurrohman <nafisrdn@gmail.com>
2023-01-04 10:58:11 +0700

    add get all useres

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -5,23 +5,36 @@ const {
 } = require("../configs/confluence.config");
 
 async function getAllUsers() {
-  const res = await axios.post(
-    `${CONFLUENCE_API_URL}/rest/confluenceuserexport/1.0/search`,
-    {
-      searchString: "",
-      activeUsers: true,
-      inActiveUsers: true,
-      pageSize: 20,
-      offset: 280,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${CONFLUENCE_API_TOKEN}`,
-      },
-    }
-  );
+  let users = [];
+  let offset = 0;
+  let pageSize = 20;
+  let hasNextPage = true;
 
-  return res.data;
+  while (hasNextPage) {
+    const res = await axios.post(
+      `${CONFLUENCE_API_URL}/rest/confluenceuserexport/1.0/search`,
+      {
+        searchString: "",
+        activeUsers: true,
+        inActiveUsers: true,
+        pageSize: pageSize,
+        offset: offset,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${CONFLUENCE_API_TOKEN}`,
+        },
+      }
+    );
+
+    const data = res.data;
+
+    users.push(data.users);
+    hasNextPage = data.hasNextPage;
+    offset += pageSize;
+  }
+
+  return users;
 }
 
 module.exports = {
